705415fc6cf14d8caa2e3197eca54446889081c1
Alberto Rodrigues <allio.professional@gmail.com>
2024-10-14 21:35:28 -0300

    feat: creating searchbar and making it usable

--- technology-news/src/app/page.tsx
+++ technology-news/src/app/page.tsx
@@ -1,9 +1,42 @@
+"use client"
+
 import NavBar from "@/components/NavBar";
+import { articleListService, ArticlesListModal } from "@/services/ArticlesService";
+import React from "react";
 
 export default function Home() {
+  const [searchInput, setSearchInput] = React.useState("")
+  const [search, setSearch] = React.useState("a")
+  const [page, setPage] = React.useState(1)
+  const [articles, setArticles] = React.useState<ArticlesListModal>()
+
+  const fetchListOfArticles = async () => {
+    const response = await articleListService.getArticles(search, page)
+
+    if (response._tag === "Left") {
+      return console.log(response.left)
+    }
+
+    setArticles(response.right)
+  }
+
+  React.useEffect(() => {
+    fetchListOfArticles()
+  }, [search, page])
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
+  }
+  
+
   return (
-    <>
-      <NavBar backButtonIsVisible={false}/>
-    </>
+    <div className="flex flex-col gap-96">
+      <NavBar 
+        backButtonIsVisible={false} 
+        searchInputValue={searchInput}
+        searchSetInputValue={setSearchInput}
+        searchHandleInputChange={handleInputChange}
+      />
+    </div>
   );
 }
